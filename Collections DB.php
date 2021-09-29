a0372c784c3685a3c803f5f192e600f658511208
damongray <dj2k15@gmail.com>
2021-09-29 15:36:47 +0100

    stripped back table of info

--- Collections DB.php
+++ Collections DB.php
@@ -4,35 +4,16 @@ $db = new PDO('mysql:host=db; dbname=collections', 'root', 'password');
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query=$db->prepare("SELECT `brewery`, `beer`, `location`, `style`, `abv`, `ibu`, `colour` FROM `breweries`;");
+$query=$db->prepare("SELECT `brewery`, `beer`, `location`, `style`, `abv`, `ibu`, `colour`, `description` FROM `breweries`;");
 
 $query->execute();
 
-$result=$query->fetchAll();
+$result = $query->fetchAll();
 
-$breweries = array("brewery", "beer", "location", "style", "abv", "ibu", "colour");
-$array_length = count($breweries);
-
-
-?>
-
-<!DOCTYPE html>
-<head lang="en">
-   <meta name="viewport" content="width=device-width", initial-scale="1.0">
-    <link rel="stylesheet" href="" />
-    <link rel="stylesheet" href="" />
-    <title>The Taproom</title>
-</head>
-<body>
-<h1 class="title">The Taproom</h1>
-<div class="The Collection">The Collection</div>
+echo '<ul>';
+foreach ($result as $breweries) {
+    echo '<li>' . $breweries['brewery'] . ' - - ' . $breweries['beer'] . ' - - ' . $breweries['location'] . ' - - ' . $breweries['style']  . ' - - ' . $breweries['abv'] . ' - - ' . $breweries['ibu'] . ' - - ' . $breweries['colour'] . ' - - ' . $breweries['description'] . ' - - ' . '</li>';
+}
+echo '</ul>';
 
 
-</body>
-
-<label for="breweries">Choose a Brewery:</label>
-<input list="breweries" id="brewery name="brewery” />
-
-<datalist id="breweries">
-</datalist>
-</html>
